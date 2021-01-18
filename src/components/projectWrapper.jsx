911a7ac3ff5f2c03eb442d7a7b62937d1d1a7450
Luke van Aardt <vanaardtluke@gmail.com>
2021-01-18 07:06:07 +0200

    git test

---
+++ src/components/projectWrapper.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ProjectsWrapper extends Component {
+    state = { 
+
+    }
+    
+    render() { 
+        return ( 
+            <section>
+
+            </section> 
+        );
+    }
+}
+ 
+export default ProjectsWrapper;
