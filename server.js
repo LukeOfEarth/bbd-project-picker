0935aac9e1c5ebc002e51ade714aafcb5670a5fc
Luke van Aardt <vanaardtluke@gmail.com>
2021-01-28 20:54:28 +0200

    getting ready for deploy

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const sessionUtil = require('./utils/sessions');
 const votesUtil = require('./utils/votes');
-
+const path = require('path');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
@@ -12,6 +13,28 @@ const io = require('socket.io')(http, {
       credentials: true
     }
   });
+
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://https://bbd-project-picker.herokuapp.com/']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+})};
 
 io.on('connection', (socket) => {
     const id = socket.handshake.query.id;
