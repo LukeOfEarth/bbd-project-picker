1013b82d9d1bcb442d7b3d38987ce9d20cbe0525
Luke van Aardt <vanaardtluke@gmail.com>
2021-01-28 20:56:13 +0200

    added env port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
     cors: {
